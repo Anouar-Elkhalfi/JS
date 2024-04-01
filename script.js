efd096394c5bb0da74fa724157e0cd07b57c4847
anouar elkhalfi <anouarelkhalfi@gmail.com>
2024-04-01 11:04:22 +0200

    Initial commit

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+console.log("Hello World") // afficher Hello World
+// let xp; //déclatrer une variable
+let xp = 0;//Initialize your xp variable to have a value of 0, a number.
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;//camelCase (multiple words)
+let fighting;
+let monsterHealth;
+let varString = "stick";// variable as a string
+let inventory = ["stick","dagger","sword"]; // variable as array of strings
